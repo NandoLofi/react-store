0a406992b5890deef78b89f14d1ac06d722ebdc1
Fernando Galvan <n.brighter00@gmail.com>
2022-08-19 12:27:14 -0500

    adding product details dynamically, fixing error

--- react-store/src/screens/HomeScreen.jsx
+++ react-store/src/screens/HomeScreen.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import "./HomeScreen.css"
 import Product from '../components/Product'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { getProducts as listProducts} from "../components/redux/actions/productActions"
+
+
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const getProducts = useSelector(state => state.getProducts )
+  const { products, loading, error } = getProducts
+
+  useEffect(()=> {
+
+  })
+
   return (
     <div className='homescreen'>
         <h2 className='homescreen__title'>Latest Products</h2>
