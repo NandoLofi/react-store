1ad9d7a258b4d14817f757d1678ab009edce83e4
Fernando Galvan <n.brighter00@gmail.com>
2022-08-19 11:28:52 -0500

    fixed redux reducer error

--- react-store/src/components/redux/store.js
+++ react-store/src/components/redux/store.js
@@ -1,16 +1,12 @@
-import { configureStore, combineReducers, applyMiddleware} from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { configureStore, combineReducers} from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cartReducers"
 
 const reducer = combineReducers({
     cart: cartReducer
 })
-const middleware = [thunk]
 
-const store = configureStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+ const store = configureStore({
+    reducer: {},
+ })
 
 export default store;
